37ecff403d35f2e570854f3b5efb4632004af41d
KahrenA <KahrenA@gmail.com>
2017-12-22 13:01:00 -0800

    Add files via upload

--- CarND-Capstone/ros/src/styx/bridge.py
+++ CarND-Capstone/ros/src/styx/bridge.py
@@ -19,6 +19,7 @@ from io import BytesIO
 import base64
 
 import math
+import random
 
 TYPE = {
     'bool': Bool,
@@ -36,155 +37,322 @@ TYPE = {
 
 
 class Bridge(object):
-    def __init__(self, conf, server):
-        rospy.init_node('styx_server')
-        self.server = server
-        self.vel = 0.
-        self.yaw = None
-        self.angular_vel = 0.
-        self.bridge = CvBridge()
+	
 
-        self.callbacks = {
-            '/vehicle/steering_cmd': self.callback_steering,
-            '/vehicle/throttle_cmd': self.callback_throttle,
-            '/vehicle/brake_cmd': self.callback_brake,
-        }
+	#------------------------------------------------------
+	def __init__(self, conf, server):
 
-        self.subscribers = [rospy.Subscriber(e.topic, TYPE[e.type], self.callbacks[e.topic])
-                            for e in conf.subscribers]
+		rospy.init_node('styx_server', log_level=rospy.DEBUG)
 
-        self.publishers = {e.name: rospy.Publisher(e.topic, TYPE[e.type], queue_size=1)
+		self.server = server
+		self.vel = 0.
+		self.yaw = None
+		self.angular_vel = 0.
+		self.bridge = CvBridge()
+
+		self.callbacks = { '/vehicle/steering_cmd': self.callback_steering,
+          						 '/vehicle/throttle_cmd': self.callback_throttle,
+						           '/vehicle/brake_cmd': self.callback_brake,
+								     }
+
+		self.subscribers = [rospy.Subscriber(e.topic, TYPE[e.type], 
+															self.callbacks[e.topic]) for e in conf.subscribers]
+
+		self.publishers = {e.name: rospy.Publisher(e.topic, TYPE[e.type], queue_size=1)
                            for e in conf.publishers}
 
-    def create_light(self, x, y, z, yaw, state):
-        light = TrafficLight()
+		rospy.logwarn("Bridge:__init__Done!")
+ 
+	#--------------------------------------------------------------------------
+	# called from publish_traffic
+	# creates a TrafficLight message 
+	#--------------------------------------------------------------------------
+	def create_light(self, x, y, z, yaw, state):
 
-        light.header = Header()
-        light.header.stamp = rospy.Time.now()
-        light.header.frame_id = '/world'
+#		rospy.logdebug("In Bridge:create_light")
 
-        light.pose = self.create_pose(x, y, z, yaw)
-        light.state = state
+		light = TrafficLight()
+		light.header = Header()
+		light.header.stamp = rospy.Time.now()
+		light.header.frame_id = '/world'
 
-        return light
+		light.pose = self.create_pose(x, y, z, yaw)
+		light.state = state
 
-    def create_pose(self, x, y, z, yaw=0.):
-        pose = PoseStamped()
+		return light
 
-        pose.header = Header()
-        pose.header.stamp = rospy.Time.now()
-        pose.header.frame_id = '/world'
+ 
+	#---------------------------------------------------------------------------
+	# Called from publish_odometry & publish_obstacles
+	# Create a PoseStamped message and init with data provided
+	#---------------------------------------------------------------------------
+	def create_pose(self, x, y, z, yaw=0.):
 
-        pose.pose.position.x = x
-        pose.pose.position.y = y
-        pose.pose.position.z = z
+		pose = PoseStamped()
 
-        q = tf.transformations.quaternion_from_euler(0., 0., math.pi * yaw/180.)
-        pose.pose.orientation = Quaternion(*q)
+		pose.header = Header()
+		pose.header.stamp = rospy.Time.now()
+		pose.header.frame_id = '/world'
 
-        return pose
+		pose.pose.position.x = x
+		pose.pose.position.y = y
+		pose.pose.position.z = z
 
-    def create_float(self, val):
-        fl = Float()
-        fl.data = val
-        return fl
+		q = tf.transformations.quaternion_from_euler(0., 0., math.pi * yaw/180.)
+		pose.pose.orientation = Quaternion(*q)
 
-    def create_twist(self, velocity, angular):
-        tw = TwistStamped()
-        tw.twist.linear.x = velocity
-        tw.twist.angular.z = angular
-        return tw
+#		rospy.logdebug("pose.x=%f; pose_y=%f; pose_z=%f", x, y, z)
 
-    def create_steer(self, val):
-        st = SteeringReport()
-        st.steering_wheel_angle_cmd = val * math.pi/180.
-        st.enabled = True
-        st.speed = self.vel
-        return st
-
-    def calc_angular(self, yaw):
-        angular_vel = 0.
-        if self.yaw is not None:
-            angular_vel = (yaw - self.yaw)/(rospy.get_time() - self.prev_time)
-        self.yaw = yaw
-        self.prev_time = rospy.get_time()
-        return angular_vel
-
-    def create_point_cloud_message(self, pts):
-        header = Header()
-        header.stamp = rospy.Time.now()
-        header.frame_id = '/world'
-        cloud_message = pcl2.create_cloud_xyz32(header, pts)
-        return cloud_message
-
-    def broadcast_transform(self, name, position, orientation):
-        br = tf.TransformBroadcaster()
-        br.sendTransform(position,
-            orientation,
-            rospy.Time.now(),
-            name,
-            "world")
-
-    def publish_odometry(self, data):
-        pose = self.create_pose(data['x'], data['y'], data['z'], data['yaw'])
-
-        position = (data['x'], data['y'], data['z'])
-        orientation = tf.transformations.quaternion_from_euler(0, 0, math.pi * data['yaw']/180.)
-        self.broadcast_transform("base_link", position, orientation)
-
-        self.publishers['current_pose'].publish(pose)
-        self.vel = data['velocity']* 0.44704
-        self.angular = self.calc_angular(data['yaw'] * math.pi/180.)
-        self.publishers['current_velocity'].publish(self.create_twist(self.vel, self.angular))
+		return pose
 
 
-    def publish_controls(self, data):
-        steering, throttle, brake = data['steering_angle'], data['throttle'], data['brake']
-        self.publishers['steering_report'].publish(self.create_steer(steering))
-        self.publishers['throttle_report'].publish(self.create_float(throttle))
-        self.publishers['brake_report'].publish(self.create_float(brake))
+	#---------------------------------------------------------------------------
+	# Called from publish_controls 
+	# Creates a Float object
+	#---------------------------------------------------------------------------
+	def create_float(self, val):
 
-    def publish_obstacles(self, data):
-        for obs in data['obstacles']:
-            pose = self.create_pose(obs[0], obs[1], obs[2])
-            self.publishers['obstacle'].publish(pose)
-        header = Header()
-        header.stamp = rospy.Time.now()
-        header.frame_id = '/world'
-        cloud = pcl2.create_cloud_xyz32(header, data['obstacles'])
-        self.publishers['obstacle_points'].publish(cloud)
+#		rospy.logdebug("In Bridge:create_float")
+		fl = Float()
+		fl.data = val
+		return fl
 
-    def publish_lidar(self, data):
-        self.publishers['lidar'].publish(self.create_point_cloud_message(zip(data['lidar_x'], data['lidar_y'], data['lidar_z'])))
 
-    def publish_traffic(self, data):
-        x, y, z = data['light_pos_x'], data['light_pos_y'], data['light_pos_z'],
-        yaw = [math.atan2(dy, dx) for dx, dy in zip(data['light_pos_dx'], data['light_pos_dy'])]
-        status = data['light_state']
 
-        lights = TrafficLightArray()
-        header = Header()
-        header.stamp = rospy.Time.now()
-        header.frame_id = '/world'
-        lights.lights = [self.create_light(*e) for e in zip(x, y, z, yaw, status)]
-        self.publishers['trafficlights'].publish(lights)
 
-    def publish_dbw_status(self, data):
-        self.publishers['dbw_status'].publish(Bool(data))
 
-    def publish_camera(self, data):
-        imgString = data["image"]
-        image = PIL_Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)
+	#---------------------------------------------------------------------------
+	# called from publish_odometry 
+	# Creates a TwistStamped message and inits with provided lin and ang vels
+	#--------------------------------------------------------------------------
+	def create_twist(self, velocity, angular):
 
-        image_message = self.bridge.cv2_to_imgmsg(image_array, encoding="rgb8")
-        self.publishers['image'].publish(image_message)
+#		rospy.logdebug("In Bridge:create_twist")
 
-    def callback_steering(self, data):
-        self.server('steer', data={'steering_angle': str(data.steering_wheel_angle_cmd)})
+		tw = TwistStamped()
+		tw.twist.linear.x = velocity
+		tw.twist.angular.z = angular
+		return tw
 
-    def callback_throttle(self, data):
-        self.server('throttle', data={'throttle': str(data.pedal_cmd)})
 
-    def callback_brake(self, data):
-        self.server('brake', data={'brake': str(data.pedal_cmd)})
+	#---------------------------------------------------------------------------
+	# called from publish_controls
+	# creates a SteeringREport message and inits with 
+	#---------------------------------------------------------------------------
+	def create_steer(self, val):
+
+#		rospy.logdebug("In Bridge:create_steer\n")
+
+		st = SteeringReport()
+		st.steering_wheel_angle_cmd = val * math.pi/180.
+		st.enabled = True
+		st.speed = self.vel
+		return st
+
+
+	#---------------------------------------------------------------------------
+	# called from publish_odometry
+	# calculate yaw 
+	#---------------------------------------------------------------------------
+	def calc_angular(self, yaw):
+
+#		rospy.logdebug("In Bridge:calc_angular")
+
+		angular_vel = 0.
+		if self.yaw is not None:
+			angular_vel = (yaw - self.yaw)/(rospy.get_time() - self.prev_time)
+		self.yaw = yaw
+		self.prev_time = rospy.get_time()
+		return angular_vel
+
+
+	#---------------------------------------------------------------------------
+	# called from publish_lidar
+	#---------------------------------------------------------------------------
+	def create_point_cloud_message(self, pts):
+
+#		rospy.logdebug("In Bridge:create_point_cloud_message")
+
+		header = Header()
+		header.stamp = rospy.Time.now()
+		header.frame_id = '/world'
+		cloud_message = pcl2.create_cloud_xyz32(header, pts)
+		return cloud_message
+
+
+	#--------------------------------------------------------------------------
+	# called from publish_odometry
+	# What does this do?
+	#--------------------------------------------------------------------------
+	def broadcast_transform(self, name, position, orientation):
+
+#		rospy.logdebug("In Bridge:broadcast_transform\n")
+
+		br = tf.TransformBroadcaster()
+		br.sendTransform(position, orientation,rospy.Time.now(), name, "world")
+
+ 
+	#--------------------------------------------------------------------------
+	# called from server.telemetry 
+	# publish current_pose and current_velocity
+	#--------------------------------------------------------------------------
+	def publish_odometry(self, data):
+
+		pose = self.create_pose(data['x'], data['y'], data['z'], data['yaw'])
+
+		position = (data['x'], data['y'], data['z'])
+		orientation = tf.transformations.quaternion_from_euler(0, 0, 
+																													math.pi * data['yaw']/180.)
+		self.broadcast_transform("base_link", position, orientation)
+
+		#.............................................
+		self.publishers['current_pose'].publish(pose)
+		#.............................................
+
+		self.vel = data['velocity']* 0.44704
+		self.angular = self.calc_angular(data['yaw'] * math.pi/180.)
+		#..........................................................................
+		self.publishers['current_velocity'].publish(self.create_twist(self.vel, 
+																																	self.angular))
+		#...........................................................................
+
+		#rospy.logdebug("Bridge:PubO incoming - lin_vel = %f, ang_vel = %f\n", 
+		#													self.vel, self.angular)
+
+
+	#-----------------------------------------------------------------------------------
+	# called from server.control 
+	# publish steering, throttle and brake messages
+	#-----------------------------------------------------------------------------------
+	def publish_controls(self, data):
+
+		steering, throttle, brake = data['steering_angle'], data['throttle'], \
+																data['brake']
+
+		#.......................................................................
+		self.publishers['steering_report'].publish(self.create_steer(steering))
+		self.publishers['throttle_report'].publish(self.create_float(throttle))
+		self.publishers['brake_report'].publish(self.create_float(brake))
+		#.......................................................................
+
+		rospy.logdebug("Incoming - steering angle = %s; throttle = %s, brake = %s\n" , 
+												steering, throttle, brake) 
+
+
+	#------------------------------------------------------------------------------------
+	# called from server.obstacle 
+	# create pose for each obstacle and publish them
+	# create cloud and publish.... what's a cloud? 
+	#------------------------------------------------------------------------------------
+	def publish_obstacles(self, data):
+
+#		rospy.logdebug("In Bridge:publish_obstacles")
+
+		for obs in data['obstacles']:
+			pose = self.create_pose(obs[0], obs[1], obs[2])
+			self.publishers['obstacle'].publish(pose)
+
+		header = Header()
+		header.stamp = rospy.Time.now()
+		header.frame_id = '/world'
+		cloud = pcl2.create_cloud_xyz32(header, data['obstacles'])
+		#.................................................
+		self.publishers['obstacle_points'].publish(cloud)
+		#.................................................
+
+
+	#-----------------------------------------------------------------------------------
+	# called from server.obstacle or lidar
+	#-----------------------------------------------------------------------------------
+	def publish_lidar(self, data):
+
+#		rospy.logdebug("In Bridge:publish_lidar")
+
+		#...........................................................................
+		self.publishers['lidar'].publish(
+												self.create_point_cloud_message(zip(data['lidar_x'], 
+																			  										 data['lidar_y'], 
+																														 data['lidar_z'])))
+		#...........................................................................
+
+
+	#------------------------------------------------------------------------------------
+	# Called from server.trafficlights
+	#------------------------------------------------------------------------------------
+	def publish_traffic(self, data):
+
+#		rospy.logdebug("In Bridge:publish_traffic")
+	
+		x, y, z = data['light_pos_x'], data['light_pos_y'], data['light_pos_z'],
+
+		yaw = [math.atan2(dy, dx) for dx, dy in zip(data['light_pos_dx'], 
+																								data['light_pos_dy'])]
+		status = data['light_state']
+
+		lights = TrafficLightArray()
+		header = Header()
+		header.stamp = rospy.Time.now()
+		header.frame_id = '/world'
+		lights.lights = [self.create_light(*e) for e in zip(x, y, z, yaw, status)]
+		#................................................
+		self.publishers['trafficlights'].publish(lights)
+		#................................................
+ 
+	#---------------------------------------------------------------------------
+	# called from server.telemetry
+	#---------------------------------------------------------------------------
+	def publish_dbw_status(self, data):
+
+		rospy.logdebug("In Bridge:publish_dbw_status")
+		self.publishers['dbw_status'].publish(Bool(data))
+
+
+	#---------------------------------------------------------------------------
+	# called from server.image
+	# --------------------------------------------------------------------------
+	def publish_camera(self, data):
+
+		# reduce publishing frequency
+		if random.uniform(0, 1) > 0.1 :
+#			rospy.logwarn("will not publish image******************************************")
+			return
+
+		imgString = data["image"]
+		image = PIL_Image.open(BytesIO(base64.b64decode(imgString)))
+		image_array = np.asarray(image)
+
+		image_message = self.bridge.cv2_to_imgmsg(image_array, encoding="rgb8")
+		#...............................................
+		self.publishers['image'].publish(image_message)
+		#...............................................
+ 
+	#---------------------------------------------------------------------------------
+	# Called from dbw_node publish (steer)
+	# send steering data via send in server
+	#---------------------------------------------------------------------------------
+	def callback_steering(self, data):
+
+		#rospy.logdebug("In Bridge:callback_steering")
+		self.server('steer', data={'steering_angle': str(data.steering_wheel_angle_cmd)})
+
+ 
+	#-------------------------------------------------------------------------------
+	# Called from dbw_node publish(throttle)
+	# send throttle data via send in server
+	#-------------------------------------------------------------------------------
+	def callback_throttle(self, data):
+
+		#rospy.logdebug("In Bridge:callback_throttle")
+		self.server('throttle', data={'throttle': str(data.pedal_cmd)})
+
+
+	#-------------------------------------------------------------------------------
+	# Called from dbw_node publish(brake) 
+	#	send brake data via send in server
+	#-------------------------------------------------------------------------------
+	def callback_brake(self, data):
+
+		#rospy.logdebug("In Bridge:callback_brake")
+		self.server('brake', data={'brake': str(data.pedal_cmd)})
+
+
--- CarND-Capstone/ros/src/styx/server.py
+++ CarND-Capstone/ros/src/styx/server.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-import eventlet
-eventlet.monkey_patch(socket=True, select=True, time=True)
-
-import eventlet.wsgi
 import socketio
+import eventlet
+import eventlet.wsgi
 import time
 from flask import Flask, render_template
 
 from bridge import Bridge
 from conf import conf
+
+import rospy
 
 sio = socketio.Server()
 app = Flask(__name__)
@@ -17,48 +17,77 @@ msgs = []
 
 dbw_enable = False
 
+#------------------------------------------------------
 @sio.on('connect')
 def connect(sid, environ):
     print("connect ", sid)
 
+#------------------------------------------------------
 def send(topic, data):
-    s = 1
-    msgs.append((topic, data))
-    #sio.emit(topic, data=json.dumps(data), skip_sid=True)
+	s = 1
+	msgs.append((topic, data))
+	rospy.logdebug("In send. topic = %s; data = %s", topic, data)
+	#sio.emit(topic, data=json.dumps(data), skip_sid=True)
 
+#-------------------------------------------------------
 bridge = Bridge(conf, send)
 
+#------------------------------------------------------
 @sio.on('telemetry')
 def telemetry(sid, data):
-    global dbw_enable
-    if data["dbw_enable"] != dbw_enable:
-        dbw_enable = data["dbw_enable"]
-        bridge.publish_dbw_status(dbw_enable)
-    bridge.publish_odometry(data)
-    for i in range(len(msgs)):
-        topic, data = msgs.pop(0)
-        sio.emit(topic, data=data, skip_sid=True)
 
+	global dbw_enable
+#	rospy.logdebug("In telemetry: data[dbw_enable] = %d; and dbw_enable flag = %d\n", 
+#										data["dbw_enable"], dbw_enable)
+
+	if data["dbw_enable"] != dbw_enable:
+		dbw_enable = data["dbw_enable"]
+		bridge.publish_dbw_status(dbw_enable)
+
+	bridge.publish_odometry(data)
+
+	for i in range(len(msgs)):
+		topic, data = msgs.pop(0)
+		sio.emit(topic, data=data, skip_sid=True)
+		#rospy.logwarn("In telemetry-send. topic = %s; data = %s", topic, data)
+
+
+#------------------------------------------------------
 @sio.on('control')
 def control(sid, data):
-    bridge.publish_controls(data)
+	rospy.logdebug("In server:control\n")
+	bridge.publish_controls(data)
 
+
+#------------------------------------------------------
 @sio.on('obstacle')
 def obstacle(sid, data):
-    bridge.publish_obstacles(data)
+	rospy.logdebug("In server:obstacle\n")
+	bridge.publish_obstacles(data)
 
+
+#------------------------------------------------------
 @sio.on('lidar')
 def obstacle(sid, data):
-    bridge.publish_lidar(data)
+	rospy.logdebug("In server:lidar\n")
+	bridge.publish_lidar(data)
 
+
+#------------------------------------------------------
 @sio.on('trafficlights')
 def trafficlights(sid, data):
-    bridge.publish_traffic(data)
+	rospy.logdebug("In server:trafficlights\n")
+	bridge.publish_traffic(data)
 
+
+#------------------------------------------------------
 @sio.on('image')
 def image(sid, data):
-    bridge.publish_camera(data)
+	rospy.logdebug("In server:image\n")
+	bridge.publish_camera(data)
 
+
+#------------------------------------------------------
 if __name__ == '__main__':
 
     # wrap Flask application with engineio's middleware
